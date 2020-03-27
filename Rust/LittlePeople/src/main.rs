b82438643f54c32185e680f2cd9afbaa3d9e49ad
Daryl Dudey <daryljdudey@googlemail.com>
2020-03-27 23:09:44 +0000

    rust little people

---
+++ Rust/LittlePeople/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let a = 10;
+    println!("{}", a);
+    println!("Hello, world!");
+}
---
+++ Rust/LittlePeople/target/rls/debug/deps/little_people-1ff73c008a943f81.d
@@ -0,0 +1,5 @@
+/mnt/Data/Development/repos/personal/prototypes/Rust/LittlePeople/target/rls/debug/deps/little_people-1ff73c008a943f81.rmeta: src/main.rs
+
+/mnt/Data/Development/repos/personal/prototypes/Rust/LittlePeople/target/rls/debug/deps/little_people-1ff73c008a943f81.d: src/main.rs
+
+src/main.rs:
