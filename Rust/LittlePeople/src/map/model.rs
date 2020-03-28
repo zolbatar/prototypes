73f30b7f8478bef6b25e4831766aa10cb2c0f8f5
Daryl Dudey <daryljdudey@googlemail.com>
2020-03-28 23:39:50 +0000

    single voxel

--- Rust/LittlePeople/src/map/model.rs
+++ Rust/LittlePeople/src/map/model.rs
@@ -1,6 +1,6 @@
-pub const MAP_X: usize = 4;//128;
-pub const MAP_Y: usize = 4;//32;
-pub const MAP_Z: usize = 4;//128;
+pub const MAP_X: usize = 1;//128;
+pub const MAP_Y: usize = 1;//32;
+pub const MAP_Z: usize = 1;//128;
 pub const MAP_SIZE: usize = MAP_X * MAP_Y * MAP_Z;
 pub const VOXEL_MAX: i8 = 32;
 
--- Rust/LittlePeople/src/map/render.rs
+++ Rust/LittlePeople/src/map/render.rs
@@ -1,25 +1,30 @@
 use crate::map::model::{Map, MAP_SIZE, MAP_X, MAP_Y, MAP_Z, VOXEL_MAX};
 use ggez;
-use ggez::graphics;
+use ggez::graphics::{draw, DrawMode, Mesh, MeshBuilder, WHITE};
 use ggez::nalgebra as na;
 use ggez::Context;
 
-const SPACING: f32 = 10.0;
+const SPACING: f32 = 50.0;
 
 pub fn render_map(ctx: &mut Context, map: &Map, screen_width: usize, screen_height: usize) {
-    let circle = graphics::Mesh::new_circle(
+    let circle = Mesh::new_circle(
         ctx,
-        graphics::DrawMode::fill(),
+        DrawMode::fill(),
         na::Point2::new(0.0, 0.0),
         2.0,
         2.0,
-        graphics::WHITE,
+        WHITE,
     )
     .unwrap();
 
+    // Calculate steps
+    let angle = 30_f32.to_radians();
+    let x_increment = SPACING * angle.cos();
+    let y_increment = SPACING * angle.sin();
+
     // Work out position adjustment
-    let x_adjust = (screen_width as f32 / 2.0) - (MAP_X as f32 / 2.0 * SPACING);
-    let z_adjust = (screen_height as f32 / 2.0) - (MAP_Z as f32 / 2.0 * SPACING);
+    let x_adjust = (screen_width as f32 / 2.0) - (MAP_X as f32 / 2.0 * x_increment);
+    let z_adjust = (screen_height as f32 / 2.0) - (MAP_Z as f32 / 2.0 * y_increment);
 
     for x in 0..MAP_X {
         for z in 0..MAP_Z {
@@ -27,8 +32,53 @@ pub fn render_map(ctx: &mut Context, map: &Map, screen_width: usize, screen_heig
                 let index = (x * MAP_Z) + (y * MAP_X * MAP_Z);
                 let voxel = map.voxels[index];
 
-                let p = na::Point2::new(x as f32 * SPACING + x_adjust, z as f32 * SPACING + z_adjust);
-                graphics::draw(ctx, &circle, (p,)).unwrap();
+                // Position
+                let screen_x = x as f32 * x_increment + x_adjust;
+                let screen_y = z as f32 * y_increment + z_adjust;
+                let p = na::Point2::new(screen_x, screen_y);
+                //graphics::draw(ctx, &circle, (p,)).unwrap();
+
+                // Construct a MeshBuilder with all 3 visible sides
+                let mesh = MeshBuilder::new()
+                    .polygon(
+                        DrawMode::fill(),
+                        &[
+                            na::Point2::new(0.0, 0.0),
+                            na::Point2::new(0.0 - x_increment, -y_increment),
+                            na::Point2::new(0.0 - x_increment, -y_increment - SPACING),
+                            na::Point2::new(0.0, -SPACING),
+                            na::Point2::new(0.0, 0.0),
+                        ],
+                        WHITE,
+                    )
+                    .unwrap()
+                    .polygon(
+                        DrawMode::fill(),
+                        &[
+                            na::Point2::new(0.0, 0.0),
+                            na::Point2::new(0.0 + x_increment, -y_increment),
+                            na::Point2::new(0.0 + x_increment, -y_increment - SPACING),
+                            na::Point2::new(0.0, -SPACING),
+                            na::Point2::new(0.0, 0.0),
+                        ],
+                        WHITE,
+                    )
+                    .unwrap()
+                    .polygon(
+                        DrawMode::fill(),
+                        &[
+                            na::Point2::new(0.0, -SPACING),
+                            na::Point2::new(0.0 - x_increment, -y_increment - SPACING),
+                            na::Point2::new(0.0, -SPACING - SPACING),
+                            na::Point2::new(0.0 + x_increment, -y_increment - SPACING),
+                            na::Point2::new(0.0, -SPACING),
+                        ],
+                        WHITE,
+                    )
+                    .unwrap()
+                    .build(ctx)
+                    .unwrap();
+                draw(ctx, &mesh, (p,)).unwrap();
             }
         }
     }
